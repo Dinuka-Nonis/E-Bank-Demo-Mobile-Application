83c6efafd3e6711587ef2c9d402ef1ad0d2ebfa0
Dinuka Nonis <dinukanonis@gmail.com>
2026-08-10 22:24:16 +0530

    spacing fixed

--- app/src/main/java/com/example/ebank/HomeActivity.kt
+++ app/src/main/java/com/example/ebank/HomeActivity.kt
@@ -2,11 +2,22 @@
 package com.example.ebank
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val root = findViewById<View>(R.id.rootHome)
+        ViewCompat.setOnApplyWindowInsetsListener(root) { view, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = bars.top, bottom = bars.bottom)
+            insets
+        }
     }
 }
--- app/src/main/java/com/example/ebank/LoginActivity.kt
+++ app/src/main/java/com/example/ebank/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.ebank
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState )
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin = findViewById<TextView>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
