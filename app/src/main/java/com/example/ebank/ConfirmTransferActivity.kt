9412ee1f65535e348cbbf9591399353d4c08f834
Dinuka Nonis <dinukanonis@gmail.com>
2026-08-10 16:26:46 +0530

    Blank pages - 5 screens

---
+++ app/src/main/java/com/example/ebank/ConfirmTransferActivity.kt
@@ -0,0 +1,12 @@
+// ConfirmTransferActivity.kt
+package com.example.ebank
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmTransferActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_transfer)
+    }
+}
